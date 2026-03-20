43c002d9651e06643bef13b0fc0a7234bed4ead5
quavix <kenmillard2911@gmail.com>
2026-03-20 18:26:32 +0800

    Add files via upload

--- Quavix.lua
+++ Quavix.lua
@@ -380,7 +380,7 @@ spawn(function()
     wait(0.1)
     notify("Auto Detect Prefix Lag Is Fixed")
     wait(5)
-    notify("8 new words added (Updated on 3/19/2026)")
+    notify("5 New Words Added and Removing 970 Words (Updated on 3/20/2026)")
     wait(10)
     
     pcall(function()
